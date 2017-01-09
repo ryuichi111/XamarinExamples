519f71d49856a4a243e076237841d2a2b78d583d
Ryuichi Daigo <daigo@RyuichinoMacBook-Pro.local>
2017-01-09 17:35:04 +0900

    add control

---
+++ Prism/Control/UseButtonAndLabel/UseButtonAndLabel/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using UseButtonAndLabel.Views;
+
+namespace UseButtonAndLabel
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync("MainPage");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
+
---
+++ Prism/Control/UseButtonAndLabel/UseButtonAndLabel/Views/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace UseButtonAndLabel.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
