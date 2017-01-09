8c06f27782307624f50e7a45806608fd8a4c195d
Ryuichi Daigo <daigo@RyuichinoMacBook-Pro.local>
2017-01-10 01:37:49 +0900

    add UseEditor

---
+++ Prism/Control/UseEditor/UseEditor/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Windows.Input;
+
+namespace UseEditor.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+		private string labelText = "";
+		public string LabelText
+		{
+			get
+			{
+				return this.labelText;
+			}
+			set
+			{
+				this.SetProperty(ref this.labelText, value);
+			}
+		}
+
+		private string editorText = "";
+		public string EditorText
+		{
+			get
+			{
+				return this.editorText;
+			}
+			set
+			{
+				this.SetProperty(ref this.editorText, value);
+			}
+		}
+
+		public ICommand OkCommand { get; }
+		public ICommand SetTextCommand { get; }
+
+		public MainPageViewModel()
+		{
+			this.OkCommand = new DelegateCommand(this.Ok);
+			this.SetTextCommand = new DelegateCommand(this.SetText);
+		}
+
+		private void Ok()
+		{
+			this.LabelText = this.EditorText;
+		}
+
+		private void SetText()
+		{
+			this.EditorText = DateTime.Now.ToString("yyyy/MM/dd H:m:s");
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+		}
+	}
+}
+
