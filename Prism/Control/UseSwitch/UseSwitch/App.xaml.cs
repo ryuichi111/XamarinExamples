b8ac97fe7178414cddcac3de0ff95abd90ad53a7
Ryuichi Daigo <daigo@RyuichinoMacBook-Pro.local>
2017-01-10 23:59:06 +0900

    add UseSwitch

---
+++ Prism/Control/UseSwitch/UseSwitch/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Unity;
+using UseSwitch.Views;
+
+namespace UseSwitch
+{
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+		protected override void OnInitialized()
+		{
+			InitializeComponent();
+
+			NavigationService.NavigateAsync("MainPage");
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+		}
+	}
+}
+
