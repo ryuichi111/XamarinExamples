8eaf8072f45572f924123c3eb6e79ea924347db0
ryuichi daigo <daigo@knowlbo.co.jp>
2018-03-21 23:50:33 +0900

    add UseAlexCarouselViewApp

---
+++ Prism/UseAlexCarouselViewApp/UseAlexCarouselViewApp/Models/PageInfo.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace BlankApp6.ViewModels
+{
+    public class PageInfo
+    {
+        public string Name { get; set; }
+        public Color ForeColor { get; set; }
+        public Color BackColor { get; set; }
+    }
+}
---
+++ Prism/UseAlexCarouselViewApp/UseAlexCarouselViewApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Navigation;
+using Xamarin.Forms;
+using BlankApp6.ViewModels;
+
+namespace UseAlexCarouselViewApp.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<PageInfo> CarouselPageInfo { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+
+            this.CarouselPageInfo = new ObservableCollection<PageInfo>
+            {
+                new PageInfo
+                {
+                    Name = "Page1",
+                    ForeColor=Color.Yellow,
+                    BackColor= Color.Red
+                },
+                new PageInfo
+                {
+                    Name = "Page2",
+                    ForeColor=Color.Black,
+                    BackColor= Color.Yellow
+                },
+                new PageInfo
+                {
+                    Name = "Page3",
+                    ForeColor=Color.Gold,
+                    BackColor= Color.Green
+                }
+            };
+        }
+    }
+}
